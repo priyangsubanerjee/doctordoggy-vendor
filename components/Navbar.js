c56913881e77914785002d9ba3c689027f60d097
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-02-16 14:25:09 +0530

    Add session handling and update navbar based on authentication status

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 import { Icon } from "@iconify/react";
 import { Avatar } from "@nextui-org/react";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 function Navbar() {
+  const session = useSession();
   return (
     <nav className="fixed top-0 inset-x-0 z-20">
       <div className="h-12 px-6 lg:px-44 lg:h-14 flex items-center justify-between bg-white/90 backdrop-blur-2xl">
@@ -13,26 +15,28 @@ function Navbar() {
             <img src="/logoDark.png" className="lg:h-9 h-7" alt="" />
           </Link>
         </div>
-        <ul className="hidden lg:flex items-center space-x-12 text-sm">
-          <li className="cursor-pointer" onClick={() => router.push("/")}>
-            Home
-          </li>
-          <li
-            className="cursor-pointer"
-            onClick={() => Router.push("/dev/progress")}
-          >
-            Partners program
-          </li>
-          <li
-            className="cursor-pointer"
-            onClick={() => Router.push("/dev/progress")}
-          >
-            FAQs
-          </li>
-          <li onClick={() => router.push("/pets")} className="cursor-pointer">
-            Contact us
-          </li>
-        </ul>
+
+        {session.status == "authenticated" ? (
+          <ul className="hidden lg:flex items-center space-x-12 text-sm">
+            <li className="cursor-pointer" onClick={() => router.push("/")}>
+              Home
+            </li>
+            <li className="cursor-pointer">Services</li>
+            <li className="cursor-pointer">Appointments</li>
+            <li className="cursor-pointer">Doctors</li>
+            <li className="cursor-pointer">Help</li>
+          </ul>
+        ) : (
+          <ul className="hidden lg:flex items-center space-x-12 text-sm">
+            <li className="cursor-pointer" onClick={() => router.push("/")}>
+              Home
+            </li>
+            <li className="cursor-pointer">Partners program</li>
+            <li className="cursor-pointer">FAQs</li>
+            <li className="cursor-pointer">Contact us</li>
+          </ul>
+        )}
+
         <div className="flex items-center">
           <Link href="/login">
             <Avatar className="bg-neutral-100" size="sm" />
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,10 +1,57 @@
+/* eslint-disable @next/next/no-img-element */
+import { Icon } from "@iconify/react";
+import { Button, Tab, Tabs } from "@nextui-org/react";
 import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 import React from "react";
 
 function Dashboard() {
+  const session = useSession();
+  console.log(session);
   return (
-    <div className="pt-32">
-      <button onClick={() => signOut()}>Logout</button>
+    <div className="min-h-screen pt-14 bg-neutral-50">
+      <div className="max-w-6xl mx-auto bg-white px-12 pb-24">
+        <div className="relative">
+          <img
+            className="absolute inset-0 h-full w-full"
+            src="https://img.freepik.com/premium-photo/white-background-with-blue-background-word-white-it_207225-64.jpg"
+            alt=""
+          />
+          <div className="relative py-16">
+            <div className="flex text-xs items-center space-x-2 w-fit text-neutral-600 border-b border-dashed pb-2">
+              <Icon icon="solar:calendar-broken" width="16" height="16" />
+              <span>
+                {new Date().toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}
+              </span>
+            </div>
+            <h2 className="text-3xl font-poppins font-semibold mt-7">
+              {session?.data?.user?.name}
+            </h2>
+            <div className="flex items-center space-x-2 mt-6">
+              <div className="flex items-center space-x-1 bg-neutral-50 px-3 py-1 rounded-full">
+                <Icon icon="fluent:call-16-regular" width="24" height="24" />
+                <span className="text-sm text-neutral-700">9647045453</span>
+              </div>
+              <div className="flex items-center space-x-2 bg-neutral-50 px-3 py-1 rounded-full">
+                <Icon icon="iconoir:mail" width="22" height="22" />
+                <span className="text-sm text-neutral-700">
+                  priyangsu26@gmail.com
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-4 w-full gap-3">
+          <div className="h-32 bg-neutral-50 rounded"></div>
+          <div className="h-32 bg-neutral-50 rounded"></div>
+          <div className="h-32 bg-neutral-50 rounded"></div>
+          <div className="h-32 bg-neutral-50 rounded"></div>
+        </div>
+      </div>
     </div>
   );
 }
