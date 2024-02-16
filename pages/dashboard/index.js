61fb8e59b499b4013e7eaa622eb40c60ae1198ff
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-02-16 20:57:38 +0530

    Update create service button styling

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -50,7 +50,7 @@ function Dashboard() {
     return (
       <div className="grid md:grid-cols-2 gap-5 lg:grid-cols-3 mt-6 md:mt-10 px-5 md:px-12">
         <ServiceCard />
-        <div className="flex items-center justify-center bg-neutral-50 rounded-lg cursor-pointer">
+        <div className="flex items-center justify-center bg-neutral-50 rounded-lg cursor-pointer py-5">
           <Icon icon="fluent:cube-add-20-regular" width="32" height="32" />
           <span className="ml-3 text-sm font-medium text-neutral-700">
             Create service
